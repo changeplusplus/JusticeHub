8b47946bf7c059a117eb3617624632251630b71d
Jarrett Perkins <jarrettperkins@Jarretts-MacBook-Pro.local>
2019-02-12 22:01:35 -0600

    Added/cleane up text inputs, added picker, added button

--- Screens/Auth/SignUp.js
+++ Screens/Auth/SignUp.js
@@ -1,43 +1,69 @@
 import React, { Component } from 'react';
 import * as firebase from 'firebase';
-import { AppRegistry, View, KeyboardAvoidingView, Text, TextInput,
+import { Alert, AppRegistry, View, KeyboardAvoidingView, Text, TextInput,
           Button, Picker } from 'react-native';
 import { InputBlock } from "../../Components/InputBlock";
 import DataStorage from '../../DataStorage';
 
 export default class SignUp extends Component {
+
+  _onPressButton() {
+    Alert.alert('You tapped the button!')
+  }
+
   constructor(props) {
     super(props);
     this.state = {
-      fullName: 'Full Name',
-      password: 'Password',
-      phone: 'Phone Number',
+      fullName: "Full Name",
+      password: "Username",
+      phone: "Password",
+      isLawyer: false
     };
   }
 
   render() {
     return (
-      <View style={{ flex: 1 , padding: 10}}>
-        <KeyboardAvoidingView behavior='padding'>
+      <View style = {{flex: 1, alignItems: "center", justifyContent: "center"}}>
+        <KeyboardAvoidingView behavior='padding' style = {{flex: 1, padding: 10}}>
 
           <TextInput
                       style = {{height: 40}}
-                      placeholder="Hmm"
-                      onSubmitEditing={(fullName) => this.setState({fullName})}
+                      placeholder="Full Name"
+                      onChangeText={(fullName) => this.setState({fullName})}
           />
-
-          <Text>{this.state.fullName}</Text>
+          <TextInput
+                      style = {{height: 40}}
+                      placeholder="Username"
+                      onChangeText={(fullName) => this.setState({fullName})}
+          />
+          <TextInput
+                      style = {{height: 40}}
+                      placeholder="Password"
+                      onChangeText={(fullName) => this.setState({fullName})}
+          />
           <Picker
-            selectedValue={this.state.isLawyer}
-            onValueChange={(itemValue) => this.setState({isLawyer: itemValue})}>
-
-            <Picker.Item label='Lawyer' value={true} />
-            <Picker.Item label='Client' value={false} />
+                      selectedValue={this.state.isLawyer}
+                      style={{height: 1, width: 100}}
+                      onValueChange={(itemValue, itemIndex) =>
+                        this.setState({isLawyer: itemValue})
+                      }>
+                      <Picker.Item label="Lawyer" value={true} />
+                      <Picker.Item label="Client" value={false} />
           </Picker>
 
-          <Button onPress={this._signUp} title='Sign Up' color='blue' />
+
         </KeyboardAvoidingView>
+        <View style = {{flex: 1}}>
+          <Button
+            onPress={this._onPressButton}
+            title="Press Me"
+            color="#841584"
+          />
+        </View>
+
       </View>
+
+
     )
   }
 }
