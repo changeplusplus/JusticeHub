8f2773a099d4095c2cb5c3ca9fa5623460905369
sandbornm <michael.sandborn@vanderbilt.edu>
2019-02-18 02:35:07 -0600

    chat files

---
+++ Screens/Chat/ChatEntry.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, Button } from 'react-native';
+import * as firebase from 'firebase';
+import ChatMain from "./Screens/Chat/ChatMain";
+
+class ChatEntry extends Component {
+
+    render() {
+        const {navigate} = this.props.navigation;
+        return (
+            <View>
+                <View style={styles.button}>
+                    <Text style={styles.buttonText}>Chat</Text>
+                </View>
+
+                <Button
+                    onPress={() => navigate('ChatMain')}
+                    title="to Chat"
+                    color="#841584"
+                />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+
+    button: {
+        marginBottom: 30,
+        width: 260,
+        alignItems: 'center',
+        backgroundColor: '#b66040'
+    },
+    buttonText: {
+        padding: 20,
+        color: 'white'
+    }
+});
+
+export default ChatEntry;
