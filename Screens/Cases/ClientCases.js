ca2a94aa8d8d152dda0199876b505cf019a7cfcb
jsladerman <jsladerman@gmail.com>
2019-02-19 22:21:33 -0600

    Fixed issue where it wasn't actually pulling the input data

--- Screens/Cases/ClientCases.js
+++ Screens/Cases/ClientCases.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import {View, Text, Button, TextInput} from 'react-native';
 import {ModalProps} from "react-native-modal";
 import * as firebase from 'firebase';
-import {InputBlock} from "../../Components/InputBlock";
 
 //TODO Implement Modal to make add case pop up from the case list
 export default class ClientCases extends Component {
-    state = {
-        caseName: '',
-        caseDetails: ''
-    };
+    constructor(props){
+        super(props);
+        this.state = {
+            caseName: '',
+            caseDetails: ''
+        };
+    }
+
 
     render() {
         return (
@@ -19,13 +22,16 @@ export default class ClientCases extends Component {
                 justifyContent: 'center',
                 alignItems: 'center'
             }}>
-                <InputBlock item='Name of Case'
-                            state='caseName'
-                            onChangeText={(caseName) => this.state.caseName = caseName}/>
-                <InputBlock item='Case Details'
-                            state='caseDetails'
-                            onChangeText={(caseDetails) => this.state.caseDetails = caseDetails}/>
-
+                <Text>Name of Case</Text>
+                <TextInput
+                    placeholder="title"
+                    onChangeText={(text) => this.state.caseName = text}
+                />
+                <Text>Case Details</Text>
+                <TextInput
+                    placeholder="description"
+                    onChangeText={(text) => this.state.caseDetails = text}
+                />
                 <Button onPress={this.submitCase} title='Submit Case' />
                 <Button onPress={this.clearCases} title='Clear Cases' />
             </View>
