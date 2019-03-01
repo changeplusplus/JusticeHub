0b7800188e3d7cabc499cd8e254c04a845901e85
jsladerman <jsladerman@gmail.com>
2019-03-01 16:55:52 -0600

    Added modal transition that will be case add/edit feature

--- Screens/Cases/ClientCases.js
+++ Screens/Cases/ClientCases.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
-import {View, Text, Button, TextInput} from 'react-native';
-import {ModalProps} from "react-native-modal";
+import {View, Text, Button, TextInput, TouchableOpacity} from 'react-native';
+import Modal from "react-native-modal";
 import * as firebase from 'firebase';
 
 //TODO Implement Modal to make add case pop up from the case list
 export default class ClientCases extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            caseName: '',
-            caseDetails: ''
-        };
-    }
+    // constructor(props){
+    //     super(props);
+    //     this.state = {
+    //         caseName: '',
+    //         caseDetails: ''
+    //     };
+    // }
+    state = {
+        isModalVisible: false,
+        caseName: '',
+        caseDetails: ''
+    };
 
-
+    _toggleModal = () =>
+        this.setState({ isModalVisible: !this.state.isModalVisible });
     render() {
         return (
             <View style={{
@@ -22,15 +28,26 @@ export default class ClientCases extends Component {
                 justifyContent: 'center',
                 alignItems: 'center'
             }}>
+                <TouchableOpacity onPress={this._toggleModal}>
+                    <Text>Show Modal</Text>
+                </TouchableOpacity>
+                <Modal isVisible={this.state.isModalVisible} animationIn='fadeInRight'>
+                    <View style={{ flex: 1 }}>
+                        <Text>Hello!</Text>
+                        <TouchableOpacity onPress={this._toggleModal}>
+                            <Text>Hide me!</Text>
+                        </TouchableOpacity>
+                    </View>
+                </Modal>
                 <Text>Name of Case</Text>
                 <TextInput
                     placeholder="title"
-                    onChangeText={(text) => this.state.caseName = text}
+                    onChangeText={(text) => this.setState({caseName: text})}
                 />
                 <Text>Case Details</Text>
                 <TextInput
                     placeholder="description"
-                    onChangeText={(text) => this.state.caseDetails = text}
+                    onChangeText={(text) => this.setState({caseDetails: text})}
                 />
                 <Button onPress={this.submitCase} title='Submit Case' />
                 <Button onPress={this.clearCases} title='Clear Cases' />
@@ -42,16 +59,20 @@ export default class ClientCases extends Component {
         const { caseName, caseDetails } = this.state;
         let user = firebase.auth().currentUser;
         let caseTest = firebase.database().ref("users/" + user.uid + "/cases/");
+        // caseTest.once('value', function(snapshot){
+        //     let test = snapshot.val()['-LZakK1a8-T3LcrxL2uV']['caseName'];
+        //     alert(test);
+        // });
         caseTest.push({
             caseName : caseName,
             caseDetails : caseDetails
         })
-            .then(() =>{
-                alert("Case added successfully!");
-            })
-            .catch((error) =>{
-                alert(error);
-            })
+        //     .then(() =>{
+        //         alert("Case added successfully!");
+        //     })
+        //     .catch((error) =>{
+        //         alert(error);
+        //     })
     };
 
     clearCases = () => {
