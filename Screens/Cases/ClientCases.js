2115a3c23f547b6115c2f9197465d7db65f75467
jsladerman <jsladerman@gmail.com>
2019-03-02 04:54:49 -0600

    Ton of styling

--- Screens/Cases/ClientCases.js
+++ Screens/Cases/ClientCases.js
@@ -1,64 +1,101 @@
 import React, { Component } from 'react';
-import {View, Text, Button, TextInput, TouchableOpacity, Image} from 'react-native';
+import {View, Text, Button, TextInput, TouchableOpacity, Image, Dimensions} from 'react-native';
 import Modal from "react-native-modal";
 import * as firebase from 'firebase';
 
 //TODO Implement Modal to make add case pop up from the case list
+const { width, height } = Dimensions.get('window');
 export default class ClientCases extends Component {
-    // constructor(props){
-    //     super(props);
-    //     this.state = {
-    //         caseName: '',
-    //         caseDetails: ''
-    //     };
-    // }
     state = {
         isModalVisible: false,
         caseName: '',
         caseDetails: ''
     };
-
-    _toggleModal = () =>
+    _toggleModal = () => {
         this.setState({ isModalVisible: !this.state.isModalVisible });
+        // alert(height-50);
+    }
     render() {
         return (
             <View style={{
                 flex: 1,
                 flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center'
+                justifyContent: 'flex-start',
+                // marginBottom: (height-100),
+                alignItems: 'center',
+                // backgroundColor: 'blue'
             }}>
-                <TouchableOpacity onPress={this._toggleModal}>
-                    <Image style={{width:50, height: 50}}
-                        source={require('./addCaseButton.png')}/>
-                </TouchableOpacity>
+                <View style={{
+                    width: width,
+                    height: 100,
+                    backgroundColor: 'blue'
+                }}>
+                    <TouchableOpacity onPress={this._toggleModal}
+                                      style={{
+                                          marginTop: 50,
+                                          marginRight: 25,
+                                          marginBottom: 15,
+                                          alignSelf: 'flex-end'
+                    }}>
+                        <Image
+                            source={require('./addCaseButton.png')}
+                            style={{
+                                width:40,
+                                height: 40}}/>
+                    </TouchableOpacity>
+                </View>
+                <View style={{
+                    flex: 1,
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    marginVertical: 50,
+                    marginHorizontal: 20,
+                    alignItems: 'center'
+
+                }}>
+                    <Text style={{
+                        fontSize:32,
+                        fontWeight:'bold',
+                        textAlign: 'center'
+                    }}>
+                        Click + sign to add a case
+                    </Text>
+                    <Button onPress={this.clearCases} title='Clear Cases' />
+                </View>
                 <Modal isVisible={this.state.isModalVisible}
                         animationIn='bounceIn'
                         animationInTiming={700}
                         hasBackdrop={false}
-                        backdropColor='blue'>
+                        backdropColor='blue'
+                        backdropOpacity={.4}
+                        onBackdropPress={this._toggleModal}>
+
                     <View style={{
                         flex: .75,
                         flexDirection: 'column',
                         justifyContent: 'center',
                         alignItems: 'center',
-                        backgroundColor: 'white'
+                        backgroundColor: 'white',
+                        borderRadius: 70,
+                        borderWidth: 0,
                     }}>
                         <Text>Name of Case</Text>
                         <TextInput
                             placeholder="title"
                             onChangeText={(text) => this.setState({caseName: text})}
                         />
-                        <Text>Case Details</Text>
+                        <Text style={{borderTopWidth:7, borderBottomWidth: 3}}>Case Details</Text>
                         <TextInput
+                            style={{flex: .5}}
+                            multiline={true}
                             placeholder="description"
+                            width={250}
+                            maxLength={400}
+                            borderWidth={1}
+                            textAlign={'center'}
                             onChangeText={(text) => this.setState({caseDetails: text})}
                         />
                         <Button onPress={this.submitCase} title='Submit Case' />
-                        <Button onPress={this.clearCases} title='Clear Cases' />
-                        <TouchableOpacity onPress={this._toggleModal}>
-                            <Text>Hide me!</Text>
-                        </TouchableOpacity>
                     </View>
                 </Modal>
             </View>
@@ -83,6 +120,7 @@ export default class ClientCases extends Component {
             .catch((error) =>{
                 alert(error);
             })
+        this._toggleModal();
     };
 
     clearCases = () => {
