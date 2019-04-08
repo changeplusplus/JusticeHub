23487d1befd38e527a709ac7d1f866afb474fc03
Jarrett Perkins <jarrettperkins@Jarretts-MacBook-Pro.local>
2019-04-07 21:44:26 -0500

    fixed incorrect value

--- Screens/Profile/SetupLawyerProfile.js
+++ Screens/Profile/SetupLawyerProfile.js
@@ -10,7 +10,7 @@ class SetupLawyerProfile extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            authorized: 'false',
+            authorized: false,
             exp: '',
             bar: '',
             firm: '',
